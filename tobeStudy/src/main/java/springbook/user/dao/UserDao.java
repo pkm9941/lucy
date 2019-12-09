c29a730de7f322ee33326ffaf2fb09c45d73b1e9
PARK.KWANGMIN <PARK.KWANGMIN@SYS2018DV071.home.e-kmall.com>
2019-12-10 08:15:07 +0900

    스키마 lucy 추가 및 h2 db 초기 설정 책임을 test에서 userDao로 위임

--- tobeStudy/src/main/java/springbook/user/dao/UserDao.java
+++ tobeStudy/src/main/java/springbook/user/dao/UserDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.DataSource;
 
@@ -13,14 +14,38 @@ public class UserDao {
 	
 	private DataSource dataSource;
 	
-	public void setDataSource(DataSource dataSource) {
+	public void setDataSource(DataSource dataSource) throws SQLException {
 		this.dataSource = dataSource;
+		Connection c = dataSource.getConnection();
+        Statement stmt = null;
+        try {
+            c.setAutoCommit(false);
+            stmt = c.createStatement();
+
+            //stmt.execute("drop user lucy;");
+            stmt.execute("create schema if not exists lucy;");
+            stmt.execute("create table if not exists lucy.users (id varchar(50) primary key, name varchar(50), password varchar(50));");
+            stmt.close();
+            c.commit();
+            
+        } catch (SQLException e) {
+            System.out.println("Exception Message " + e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+				c.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
 	}
 	
 	public void add(User user) throws SQLException {
         Connection c = dataSource.getConnection();
         
-		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+		PreparedStatement ps = c.prepareStatement("insert into lucy.users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
 		ps.setString(3, user.getPassword());
@@ -34,7 +59,7 @@ public class UserDao {
 	public User get(String id) throws SQLException {
         Connection c = dataSource.getConnection();
         
-		PreparedStatement ps = c.prepareStatement("select * from users where id =?");
+		PreparedStatement ps = c.prepareStatement("select * from lucy.users where id =?");
 		ps.setString(1, id);
 	
 		ResultSet rs = ps.executeQuery();
@@ -54,7 +79,7 @@ public class UserDao {
 	public int getCount() throws SQLException {
 		Connection c = dataSource.getConnection();
 		
-		PreparedStatement ps = c.prepareStatement("select count(*) from users");
+		PreparedStatement ps = c.prepareStatement("select count(*) from lucy.users");
 		
 		ResultSet rs = ps.executeQuery();
 		rs.next();
@@ -70,7 +95,7 @@ public class UserDao {
 	public void deleteAll() throws SQLException {
 		Connection c = dataSource.getConnection();
 		
-		PreparedStatement ps = c.prepareStatement("delete from users");
+		PreparedStatement ps = c.prepareStatement("delete from lucy.users");
 		ps.executeUpdate();
 		
 		ps.close();
--- tobeStudy/src/test/java/springbook/user/dao/UserDaoTest.java
+++ tobeStudy/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -9,7 +9,6 @@ import java.sql.Statement;
 
 import javax.sql.DataSource;
 
-import org.h2.tools.DeleteDbFiles;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.beans.BeansException;
@@ -25,7 +24,6 @@ public class UserDaoTest {
 	@BeforeClass
 	public static void beforeClass() throws BeansException, SQLException {
 		context = new GenericXmlApplicationContext("applicationContext.xml");
-		initDb((DataSource)context.getBean("dataSource"));
 	}
 	
 	@Test
@@ -51,31 +49,4 @@ public class UserDaoTest {
 		assertThat("동일한 아이디", user2.getId(), is(user.getId()));
 		assertThat("동일한 비번", user2.getPassword(), is(user.getPassword()));
 	}
-	
-	private static void initDb(DataSource dataSource) throws SQLException {
-		DeleteDbFiles.execute("~", "testdb", true); // drop db if exist 'testdb'
-        Connection c = dataSource.getConnection();
-        Statement stmt = null;
-        try {
-            c.setAutoCommit(false);
-            stmt = c.createStatement();
-
-            stmt.execute("create table users (id varchar(50) primary key, name varchar(50), password varchar(50));");
-            stmt.close();
-            c.commit();
-            
-        } catch (SQLException e) {
-            System.out.println("Exception Message " + e.getLocalizedMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-				c.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-        }
-	}
-
 }
