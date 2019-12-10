db006168eb816d85575899c74c105ec45a944bc9
PARK.KWANGMIN <PARK.KWANGMIN@SYS2018DV071.home.e-kmall.com>
2019-12-10 09:14:03 +0900

    getCount 메소드 테스트 추가
    테스트를 안 만드는 것도 위험한 일이지만,
    성의없이 테스트를 만드는 바람에, 문제가 있는 코드임에도 테스트가 성공하게 만드는 건 더 위험하다.

--- tobeStudy/src/test/java/springbook/user/dao/UserDaoTest.java
+++ tobeStudy/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 
 import javax.sql.DataSource;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.beans.BeansException;
@@ -19,34 +20,50 @@ import springbook.user.domain.User;
 
 public class UserDaoTest {
 
-	static ApplicationContext context;
+	private static ApplicationContext context;
+	private UserDao dao;
 	
 	@BeforeClass
 	public static void beforeClass() throws BeansException, SQLException {
 		context = new GenericXmlApplicationContext("applicationContext.xml");
 	}
 	
+	@Before
+	public void init() {
+		dao = context.getBean("userDao", UserDao.class);
+	}
+	
 	@Test
 	public void addAndGet() throws SQLException {
-		UserDao dao = context.getBean("userDao", UserDao.class);
-
 		dao.deleteAll();
 		int cntAfterDelete = dao.getCount();
 		assertThat("삭제 후 데이터 수 0", 0, is(cntAfterDelete));
 		
-		User user = new User();
-		user.setId("lucy");
-		user.setName("김연희");
-		user.setPassword("married");
+		User user = new User("lucy", "김연희", "married");
 
 		dao.add(user);
 		int cntAfterAdd = dao.getCount();
-		assertThat("추가 후 데이터 수 1", 1, is(cntAfterAdd));
+		assertThat("추가 후 데이터 수 1", cntAfterAdd, is(1));
 
-		User user2 = new User();
-		user2 = dao.get(user.getId());
+		User user2 = dao.get(user.getId());
 		assertThat("동일한 이름", user2.getName(), is(user.getName()));
 		assertThat("동일한 아이디", user2.getId(), is(user.getId()));
 		assertThat("동일한 비번", user2.getPassword(), is(user.getPassword()));
+	}
+	
+	@Test
+	public void getCount() throws SQLException {
+		User user1 = new User("gyume", "박성철", "springno1");
+		User user2 = new User("leegw700", "이길원", "springno2");
+		User user3 = new User("bumjin", "박범진", "springno3");
+		
+		dao.deleteAll();
+		assertThat("삭제 후 0건", dao.getCount(), is(0));
+		dao.add(user1);
+		assertThat("추가 후 1건", dao.getCount(), is(1));
+		dao.add(user2);
+		assertThat("추가 후 2건", dao.getCount(), is(2));
+		dao.add(user3);
+		assertThat("추가 후 3건", dao.getCount(), is(3));
 	}
 }
