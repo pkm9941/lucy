eec49b792218da2926e15f49bc7cf4cbaaa34f5b
noh.hyungrae <noh.hyungrae@sys2020dv144.home.e-kmall.com>
2021-03-04 15:47:58 +0900

    문제 풀이(노형래)

--- programmers/src/main/java/exercise/all/lvl1/twoNumberPlus/nohhyungrae/TwoNumberPlus.java
+++ programmers/src/main/java/exercise/all/lvl1/twoNumberPlus/nohhyungrae/TwoNumberPlus.java
@@ -1,7 +1,37 @@
 package exercise.all.lvl1.twoNumberPlus.nohhyungrae;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class TwoNumberPlus {
 	public static void main(String[] args) {
-		System.out.println("안녕하세요 노형래 입니다.");
+		int[] numbers = {2,1,3,4,1};
+		System.out.println("result : "+solution(numbers));
+	}
+	
+	public static int[] solution(int[] numbers) {
+		int[] answer = {};
+		Arrays.sort(numbers);
+		
+		List<Integer> list = new ArrayList<Integer>();
+		List<Integer> list2 = new ArrayList<Integer>();
+
+		for(int num : numbers) {
+			list.add(num);
+		}
+		
+		for(int i= 0 ; i <list.size()-1; i++) {
+            for(int j=i+1; j<list.size(); j++){
+            	if(!list2.contains(list.get(i) + list.get(j)))
+            		list2.add(list.get(i) + list.get(j)); 
+            }
+		}
+		Collections.sort(list2);
+		
+		//리스트 -> 배열로 변환 (Integer형)
+		answer = list2.stream().mapToInt(i->i).toArray();
+        return answer;
 	}
 }
