b5a092738cbb86f4e8e9b5693280c81256c543bd
noh.hyungrae <noh.hyungrae@sys2020dv144.home.e-kmall.com>
2021-03-04 16:05:29 +0900

    문제 소스 수정2(노형래)

--- programmers/src/main/java/exercise/all/lvl1/twoNumberPlus/nohhyungrae/TwoNumberPlus.java
+++ programmers/src/main/java/exercise/all/lvl1/twoNumberPlus/nohhyungrae/TwoNumberPlus.java
@@ -8,7 +8,16 @@ public class TwoNumberPlus {
 		int[] numbers = {2,1,3,4,1};
 		System.out.println("result : "+solution(numbers));
 	}
-	
+
+	/* 
+		Collection 
+		 - 컬렉션의 최고 조상인 Collection에 stream()이 정의되어 있어서 Collection의 자손인 List와 Set을 구현한 컬렉션 클래스들은 모두 stream()으로 스트림을 생성할 수 있다.
+		 
+	 	Stream
+	 	 - 스트림은 데이터 소스로 부터 데이터를 읽기만 할 뿐, 변경하지 않는다.
+		 - 스트림은 한번 사용하면 닫혀서 다시 사용할 수 없다.
+		 - 스트림은 작업을 내부 반복으로 처리한다.
+	 */
 	public static int[] solution(int[] numbers) {
 		
 		List<Integer> list = new ArrayList<Integer>();
@@ -20,9 +29,7 @@ public class TwoNumberPlus {
             }
 		}
 		
-		//Collections.sort(list2);
-		//리스트 -> 배열로 변환 (Integer형)
-		//answer = list2.stream().sorted().mapToInt(i->i).toArray();
+		list.stream().sorted().forEach(System.out::println);
         return list.stream().sorted().mapToInt(i->i).toArray();
 	}
 }
