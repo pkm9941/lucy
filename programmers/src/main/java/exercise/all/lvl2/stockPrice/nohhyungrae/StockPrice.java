1e894462f93c8d53ecd2a74d2be15f4c058909e5
noh.hyungrae <noh.hyungrae@sys2020dv144.home.e-kmall.com>
2021-03-24 08:56:36 +0900

    주식가격(노형래)

--- programmers/src/main/java/exercise/all/lvl2/stockPrice/nohhyungrae/StockPrice.java
+++ programmers/src/main/java/exercise/all/lvl2/stockPrice/nohhyungrae/StockPrice.java
@@ -3,6 +3,27 @@ package exercise.all.lvl2.stockPrice.nohhyungrae;
 public class StockPrice {
 	
 	public static void main(String[] args) {
+		int[] prices = {1,2,3,2,3};
+		
+		 System.out.println("result : "+solution(prices));
+	}
+	
+	public static int[] solution(int[] prices) {
+		int[] answer = new int[prices.length];
+
+		for(int i=0; i<prices.length-1; i++) {
+			int count = 0;
+			for(int j=i+1; j<prices.length; j++) {
+				count++;
+				if(prices[i] > prices[j]) {
+					break;
+				}
+			}
+			answer[i] = count;
+		}
+		answer[prices.length-1] = 0;
+				
+        return answer;
 	}
 	
 }
