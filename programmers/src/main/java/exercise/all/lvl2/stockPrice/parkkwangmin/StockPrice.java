ee85c885943b50425e227fbee1e3a2872a5b393b
PARK.KWANGMIN <PARK.KWANGMIN@SYS2020DV115.home.e-kmall.com>
2021-03-30 09:58:26 +0900

    주식가격 문제풀이 구현

--- programmers/src/main/java/exercise/all/lvl2/stockPrice/parkkwangmin/StockPrice.java
+++ programmers/src/main/java/exercise/all/lvl2/stockPrice/parkkwangmin/StockPrice.java
@@ -1,8 +1,34 @@
 package exercise.all.lvl2.stockPrice.parkkwangmin;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class StockPrice {
 	
 	public static void main(String[] args) {
+		StockPrice stockPrice = new StockPrice();
+		int[] prices = {1, 2, 3, 2, 3};
+
+		int[] result = stockPrice.solution(prices);
+		System.out.println(Arrays.stream(result)
+								.boxed()
+								.map(t -> String.valueOf(t))
+								.collect(Collectors.joining(",")));
+	}
+
+	private int[] solution(int[] prices) {
+		int[] result  = new int[prices.length];
+		for (int i = 0; i < prices.length; i++) {
+			int maintainedTime = 0;
+			for (int j = i + 1; j < prices.length; j++) {
+				maintainedTime++;
+				
+				if (prices[i] > prices[j])
+					break;
+			}
+			result[i] = maintainedTime;
+		}
+		return result;
 	}
 	
 }
