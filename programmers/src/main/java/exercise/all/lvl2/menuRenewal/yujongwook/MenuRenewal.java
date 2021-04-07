de687684f878ba84d9891929de3562d9ce55be32
totorys <louis86y@gmail.com>
2021-04-07 12:38:11 +0900

    commit

--- programmers/src/main/java/exercise/all/lvl2/menuRenewal/yujongwook/MenuRenewal.java
+++ programmers/src/main/java/exercise/all/lvl2/menuRenewal/yujongwook/MenuRenewal.java
@@ -1,5 +1,6 @@
 package exercise.all.lvl2.menuRenewal.yujongwook;
 
+import javax.xml.crypto.Data;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -19,6 +20,8 @@ import java.util.stream.Collectors;
  */
 public class MenuRenewal {
 
+	static Map<String, Integer> analysisMap = new HashMap<String, Integer>();
+
 	public static void main(String[] args) {
 
 		MenuRenewal mr = new MenuRenewal();
@@ -26,8 +29,9 @@ public class MenuRenewal {
 		//System.out.println(Arrays.toString(pData.orders));
 		//System.out.println(Arrays.toString(pData.course));
 
-		String[] result = mr.solution(pData.orders, pData.course);
-		System.out.println(Arrays.toString(result));
+		String[] rslt = mr.solution(pData.orders, pData.course);
+		System.out.println(Arrays.toString(rslt));
+
 	}
 
 	class Data{
@@ -55,39 +59,6 @@ public class MenuRenewal {
 		return data;
 	}
 
-	public String[] generateStr(String order, int course){
-
-		String[] result;
-		int combinationCount = (order.length() - course) + 1;
-
-		System.out.println("===========================");
-		System.out.println("# order : " + order);
-		System.out.println("# course : " + course);
-		System.out.println("# combinationCount : " + combinationCount);
-		System.out.println("===========================");
-
-		if (combinationCount > 0){
-
-			result = new String[combinationCount];
-			int beginIdx = 0;
-
-			for (int i = 0; i < combinationCount; i++) {
-
-				int endIdx = beginIdx + course;
-
-				if (endIdx < order.length() + 1){
-					System.out.println("# order : " + order + " / beginIdx : " + beginIdx + " / endIdx : " + endIdx);
-					result[i] = order.substring(beginIdx, endIdx);
-					System.out.println(result[i]);
-					beginIdx = beginIdx + 1;
-				}
-			}
-		} else {
-			result = new String[0];
-		}
-		return result;
-	}
-
 	public String[] solution(String[] orders, int[] course) {
 
 		System.out.println("===========================");
@@ -97,33 +68,80 @@ public class MenuRenewal {
 
 		MenuRenewal mr = new MenuRenewal();
 
-		Map<String, Integer> analysisMap = new HashMap<String, Integer>();
 
 		for (int i = 0; i < orders.length; i++) {
 			for (int j = 0; j < course.length; j++) {
-				String[] comb = mr.generateStr(orders[i],course[j]);
-				if (comb.length != 0){
-					for (int z = 0; z < comb.length; z++){
-						if (!analysisMap.containsKey(comb[z])){
-							analysisMap.put(comb[z], 1);
-						} else {
-							analysisMap.put(comb[z], analysisMap.get(comb[z]) + 1);
-						}
-					}
-				}
+				System.out.println("## | orders: " + orders[i] + " | course: " + course[j]);
+				mr.reculsion(Arrays.asList(orders[i].split("")), new ArrayList<String>(), 0, orders[i].length(), course[j]);
 			}
+			System.out.println();
 		}
 
-		System.out.println("##############################");
-		System.out.println(analysisMap.toString());
-		System.out.println("##############################");
+		System.out.println("-------------------------------------------------------");
+		analysisMap.forEach((k,v) -> System.out.println("key: "+k+" value:"+v));
+		System.out.println("-------------------------------------------------------");
 
-		List<String> analysisList = new ArrayList<>();
-		analysisList = analysisMap.entrySet().stream().filter(v -> v.getValue() > 1).map(Map.Entry::getKey).collect(Collectors.toList());
+		List<String> analysisList = analysisMap.entrySet().stream().filter(v -> v.getValue() > 1).map(Map.Entry::getKey).collect(Collectors.toList());
+
+		//선별된 대상 중 타 후보군에서 내포하고 있으면 제외
+
+
+
+
+
+
+
+
+
+
 		String[] answer = analysisList.toArray(new String[analysisList.size()]);
 
 		return answer;
 	}
 
+	/**
+	 * 조합 구하기
+	 * @param arr    : 기준 리스트
+	 * @param index  : 반복문 시작 인덱스
+	 * @param n      : 전체 갯수
+	 * @param r      : 뽑을 갯수
+	 */
+	public void reculsion(List<String> arr, List<String> result, int index, int n, int r) {
+
+		if (r == 0) {
+
+			System.out.println(result.toString());
+
+			String comb = result.stream().sorted().map(String::valueOf).collect(Collectors.joining());
+			if (!analysisMap.containsKey(comb)){
+				analysisMap.put(comb, 1);
+			} else {
+				analysisMap.put(comb, analysisMap.get(comb) + 1);
+			}
+
+			return;
+		}
+
+		for (int i = index; i < n; i++) {
+
+			result.add(arr.get(i));
+			reculsion(arr, result,i + 1, n, r - 1);
+			result.remove(result.size() - 1);
+		}
+	}
+
+	/**
+	 * 조합수 (not use)
+	 *
+	 * @param n
+	 * @param r
+	 * @return
+	 */
+	int combi(int n, int r){
+		if(r==0 || r==n)
+			return 1;
+		else
+			return combi(n-1, r-1) + combi(n-1, r);
+	}
 
 }
