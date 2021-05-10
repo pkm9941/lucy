e70610b43fb1ce200524d65bc9991b5f746195da
PARK.KWANGMIN <PARK.KWANGMIN@SYS2020DV115.home.e-kmall.com>
2021-05-10 09:53:22 +0900

    정확성 테스트 통과 / 효율성 테스트 실패 코드

--- programmers/src/main/java/exercise/all/lvl2/oneTwoFour/parkkwangmin/OneTwoFour.java
+++ programmers/src/main/java/exercise/all/lvl2/oneTwoFour/parkkwangmin/OneTwoFour.java
@@ -10,58 +10,72 @@ import java.util.stream.Collectors;
 public class OneTwoFour {
 	
 	public static void main(String[] args) {
-		//10 =       101; = 1 * 3*3 + 1*1 = 10; 
-		//1 = 1
-		//2 = 2
-		//3 = 4
-		Long n = 40L;
-		System.out.println("10진번 : " + n);
-		BigInteger v = BigInteger.valueOf(n);
+		int n = 500000000;
+		String base3 = convertToBase3(n);
+		
+		List<String> base124List = changeToBase124ByJari(base3);
+		
+		System.out.println("reduce");
+		String result = base124List.stream().reduce((el1, el2) -> sum(el1, el2)).get();
+		
+//		Stack<String> stack = new Stack<>();
+//		
+//		for (int i = base124List.size() - 1; i >= 0; i--) {
+//			stack.push(base124List.get(i));
+//		}
+//		
+//		String base124 = sumValueOfJari(stack);
+		
+		System.out.println("3진법   : " + base3);
+		System.out.println("base124 : " + result);
+    }
+
+	private static String convertToBase3(int numberOfBase10) {
+		System.out.println("10진법 : " + numberOfBase10);
+		BigInteger v = BigInteger.valueOf(numberOfBase10);
 		BigInteger divisor = BigInteger.valueOf(3);
-		List<Integer> base3List = new ArrayList<>();
+		List<String> base3List = new ArrayList<>();
 		
 		while(true) {
 			BigInteger[] resDivide = v.divideAndRemainder(divisor);
 			
-			base3List.add(resDivide[1].intValue());
+			base3List.add(resDivide[1].toString());
 			if (resDivide[0] == BigInteger.ZERO)
 				break;
 			
 			v = resDivide[0];
 		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = base3List.size() - 1; i >= 0; i--) {
-			int intValue = base3List.get(i);
-			sb.append(base3List.get(i));
-		}
 		
-		String base3 = sb.toString();
+		Collections.reverse(base3List);
+		
+		String base3 = base3List.stream().collect(Collectors.joining());
 		System.out.println("3진법 : " + base3);
-		
-		Stack<String> base124List = new Stack<>();
-		int jari = base3.length() + 1;
-		for (int i = 0; i < base3.length(); i++) {
-			jari--;
-			if (jari == 0) break;
+		return base3;
+	}
+
+	/**
+	 * 3진법을 124진법으로 바꾼다.
+	 * 3진법의 각 자리수마다 124진법으로 바꿨을 때 규칙성이 있으므로, 3진법의 각 자리수 단위로 124진법으로 치환하는 작업을 한다. 
+	 * 12(3) -> 4(124) + 2(124)
+	 * 100022(3) -> 22224(124) + 14(124) + 2(124)
+	 * @param base3
+	 * @return
+	 * @author 박광민
+	 * @since 2021. 5. 10.
+	 */
+	private static List<String> changeToBase124ByJari(String base3) {
+		List<String> base124List = new ArrayList<>();
+		int base3Length = base3.length();
+		for (int jari = 1; jari <= base3.length(); jari++) {
+			Integer number = Integer.valueOf(base3.substring(base3Length - jari, base3Length - jari + 1));
 			
-			Integer number = Integer.valueOf(base3.substring(i, i + 1));
+			if (number == 0) continue;
 			
 			if (jari == 1) {
 				if (number == 1) {
 					base124List.add("1");
 				} else if (number == 2) {
 					base124List.add("2");
-				}
-				break;
-			}
-			
-			if (number == 0) continue;
-			
-			if (jari == 2) {
-				if (number == 1) {
-					base124List.add("4");
-				} else if (number == 2) {
-					base124List.add("14");
 				}
 				continue;
 			}
@@ -74,7 +88,72 @@ public class OneTwoFour {
 		}
 		
 		base124List.forEach(System.out::println);
+		return base124List;
+	}
+
+	private static String sum(String el1, String el2) {
+		int passOveredNumToNextJari = 0;
+		List<String> SumedUnits = new ArrayList<>();
+		int jari = 0;
+		while (true) {
+			jari++;
+			int num1 = el1.length() >= jari ? Integer.parseInt(el1.substring(el1.length() - jari, el1.length() - jari + 1)) : 0;
+			int num2 = el2.length() >= jari ? Integer.parseInt(el2.substring(el2.length() - jari, el2.length() - jari + 1)) : 0;
+			
+			if (num1 == 0 && num2 == 0) {//자릿수 모두 더한 경우 
+				if (passOveredNumToNextJari > 0) SumedUnits.add(String.valueOf(passOveredNumToNextJari));
+				break;
+			}
+			
+			String resultSumUnit = sumUnit(num1, num2);
+			String resultSumToPassOveredNum = sumUnit(Integer.valueOf(resultSumUnit.substring(1)), passOveredNumToNextJari);
+			
+			passOveredNumToNextJari = Integer.valueOf(resultSumUnit.substring(0, 1)) + Integer.valueOf(resultSumToPassOveredNum.substring(0, 1));
+			SumedUnits.add(String.valueOf(resultSumToPassOveredNum.substring(1)));
+		}
 		
+		Collections.reverse(SumedUnits);
+		
+		String sumedNum = SumedUnits.stream().collect(Collectors.joining(""));
+		
+		System.out.println("sum : " + el1 + " + " + el2);
+		System.out.println("sumedStr : " + sumedNum);
+		
+		return sumedNum;
+	}
+
+	/**
+	 * 두 숫자(124진법 수)를 더한다.
+	 * @param num1
+	 * @param num2
+	 * @return sumedValue 덧셈 결과값 ex) 1 + 1 => 인 02, 2 + 4 => 12
+	 * @author 박광민
+	 * @since 2021. 5. 10.
+	 */
+	private static String sumUnit(int num1, int num2) {
+		if (num1 == 0 || num2 == 0) {
+			return "0" + (num1 + num2);
+		}
+		
+		int sumedNum = num1 + num2;
+		if (sumedNum == 2) {//1 + 1
+			return "02";
+		} else if (sumedNum == 3) {//1 + 2
+			return "04";
+		} else if (sumedNum == 4) {//2 + 2
+			return "11";
+		} else if (sumedNum == 5) {//1 + 4
+			return "11";
+		} else if (sumedNum == 6) {//2 + 4
+			return "12";
+		} else if (sumedNum == 8) {//4 + 4
+			return "14";
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private static String sumValueOfJari(Stack<String> base124List) {
 		String base124 = null;
 		while(true) {
 			if (base124List.isEmpty()) break;
@@ -172,8 +251,7 @@ public class OneTwoFour {
 			
 			base124List.push(sumedStr);
 		}
-		
-		System.out.println("base124 : " + base124);
-    }
+		return base124;
+	}
 
 }
