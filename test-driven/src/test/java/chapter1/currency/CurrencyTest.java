a650b0b83f661ffa4b487286ad839b40e0abbe4b
PARK.KWANGMIN <PARK.KWANGMIN@SYS2020DV115.home.e-kmall.com>
2021-06-28 07:23:02 +0900

    Dollar 부작용(연산 수행 후 값이 바뀜) 해결

--- test-driven/src/test/java/chapter1/currency/CurrencyTest.java
+++ test-driven/src/test/java/chapter1/currency/CurrencyTest.java
@@ -1,8 +1,6 @@
 package chapter1.currency;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.math.BigInteger;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -14,7 +12,7 @@ import org.junit.jupiter.api.Test;
  * 5$ x 2 = 10$ <---
  * 5$ + 10USD = 10$ ($ : USD = 1 : 2)
  * amount를 private으로 만들기
- * Dollar 부작용(side effect?)
+ * * Dollar 부작용(side effect?) <---
  * Money 반올림?
  * 
  * 어떤 금액(주가)을 어떤 수(주식의 수)에 곱한 금액을 결과로 얻을 수 있어야 한다.
@@ -44,8 +42,11 @@ class CurrencyTest {
 	@Test
 	void testMultiplication() {
 		Dollar five = new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
+		Dollar product = five.times(2);
+		assertEquals(10, product.amount);
+		
+		product = five.times(3);
+		assertEquals(15, product.amount);
 	}
 	
 }
