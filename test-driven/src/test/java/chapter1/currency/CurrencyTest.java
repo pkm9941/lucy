540a9cc10a51e4d14a877243195fd9f5298e2bcd
PARK.KWANGMIN <PARK.KWANGMIN@SYS2020DV115.home.e-kmall.com>
2021-06-30 07:23:23 +0900

    equals 구현

--- test-driven/src/test/java/chapter1/currency/CurrencyTest.java
+++ test-driven/src/test/java/chapter1/currency/CurrencyTest.java
@@ -1,6 +1,8 @@
 package chapter1.currency;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -12,11 +14,10 @@ import org.junit.jupiter.api.Test;
  * 5$ x 2 = 10$ <---
  * 5$ + 10USD = 10$ ($ : USD = 1 : 2)
  * amount를 private으로 만들기
- * * Dollar 부작용(side effect?) <---
+ * Dollar 부작용(side effect?) <---
  * Money 반올림?
- * 
- * 어떤 금액(주가)을 어떤 수(주식의 수)에 곱한 금액을 결과로 얻을 수 있어야 한다.
- * 통화가 다른 두 금액을 더해서 주어진 환율에 맞게 변한 금액을 결과로 얻을 수 있어야 한다.
+ * * equals <--
+ * hashcode
  * 
  * @author 박광민
  * @since 2021. 6. 24.
@@ -47,6 +48,12 @@ class CurrencyTest {
 		
 		product = five.times(3);
 		assertEquals(15, product.amount);
+	}
+	
+	@Test
+	void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
 	
 }
