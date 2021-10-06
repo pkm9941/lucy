825237a56b95827bb91e17162d02f75a7f083ffc
parksohyeon92 <id0830id@hanmail.net>
2021-10-06 12:36:17 +0900

    구명보트

--- programmers/src/main/java/exercise/all/lvl2/lifeboat/parksohyun/Lifeboat.java
+++ programmers/src/main/java/exercise/all/lvl2/lifeboat/parksohyun/Lifeboat.java
@@ -1,8 +1,53 @@
 package exercise.all.lvl2.lifeboat.parksohyun;
 
+import java.util.Arrays;
+
 public class Lifeboat {
 
 	public static void main(String[] args) {
+		int[] people = {10,20,30,40,50,60,100};
+		int limit =100;
+		System.out.println(solution(people,limit));
+	}
+ 
+	public static int solution2(int[] people, int limit) {
+		int answer = 0;		
+		Arrays.sort(people); //오름차순
+		
+		//20,30,50,80,90
+		int i=0;
+		for(int j=people.length-1;j>=i;j--) {
+			if(people[j]+people[i]>limit) {
+				answer++;
+			}else {
+				answer++;
+				i++;
+			}
+		}
+
+		return answer;
+	}
+
+	public static int solution(int[] people, int limit) {
+		int answer = 0;
+		
+		Arrays.sort(people); //오름차순
+		//20,30,50,80,90
+		//10,20,30,40,50,60
+		//29,49,11,23,55
+		int jLen=people.length;
+		for(int i=0;i<people.length;i++){
+			for(int j=jLen-1;j>=i;j--) {
+				if(people[j]+people[i]>limit) {
+					answer++;
+				}else {
+					jLen=j;
+					answer++;
+					break;
+				}
+			}
+		}
+		return answer;
 	}
 
 }
