486f76169bcd81d38bceb21db26a66568d2b0cc9
noh.hyungrae <noh.hyungrae@sys2020dv144.home.e-kmall.com>
2021-10-26 16:11:35 +0900

    컬러링북 1차 문제 풀이(노형래)

--- programmers/src/main/java/exercise/all/lvl2/coloringBook/nohhyungrae/ColoringBook.java
+++ programmers/src/main/java/exercise/all/lvl2/coloringBook/nohhyungrae/ColoringBook.java
@@ -1,8 +1,79 @@
 package exercise.all.lvl2.coloringBook.nohhyungrae;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class ColoringBook {
 
 	public static void main(String[] args) {
+		int m = 6;
+		int n = 4;
+		int[][] picture = {{1, 1, 1, 0}, {1, 2, 2, 0}, {1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 3}, {0, 0, 0, 3}};
+		
+		System.out.println("solution : "+solution(m,n,picture));
+		
+	}
+	
+	static Map<Integer,Integer> numMap = new LinkedHashMap<>();
+	static List<Integer> num = new ArrayList<Integer>();
+	
+	public static int[] solution(int m, int n, int[][] picture) {
+        //그림에 몇 개의 영역
+		int numberOfArea = 0;
+		//가장 큰 영역은 몇 칸
+        int maxSizeOfOneArea = 0;
+        int top = -1;
+        
+        for(int i=0; i<picture.length; i++) {
+        	recall(i, 0, picture,picture[i].length,0, top ,0);
+        }
+        
+		for(int q : numMap.keySet()) {
+			num.add(numMap.get(q));
+		}
+
+        System.out.println();
+        int[] answer = new int[2];
+        answer[0] = numberOfArea = num.size();
+        answer[1] = maxSizeOfOneArea = Collections.max(num);
+        System.out.println("answer : [ "+numberOfArea+" , "+maxSizeOfOneArea+" ]");
+        return answer;
+    }
+	
+	public static int recall(int i, int j, int[][] picture,int len, int before,int top, int result) {
+
+		if(len == j)
+			return 0;
+		
+		if(i - 1 >= 0) {
+			top = picture[i-1][j];
+		}
+		
+		if(picture[i][j] != 0) {
+			if(top != 0) {
+				if(picture[i][j] == before) {
+					numMap.put(picture[i][j], numMap.get(picture[i][j])+1);	
+				}else {
+					if(top == picture[i][j]) {
+						numMap.put(picture[i][j], numMap.get(picture[i][j])+1);	
+					}else {
+						numMap.put(picture[i][j], 1);
+					}
+				}
+			}else{
+				if(numMap.containsKey(picture[i][j])) {
+					num.add(numMap.get(picture[i][j]));
+					numMap.remove(picture[i][j]);
+					numMap.put(picture[i][j], 1);
+				}
+			}
+		}
+		
+		return recall(i,j+1, picture,len , picture[i][j], top, result);
 	}
 
 }
