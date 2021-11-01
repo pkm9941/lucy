1d4a6cfa5cfd6362b2fc478c35019aaa02d42f0e
noh.hyungrae <noh.hyungrae@sys2020dv144.home.e-kmall.com>
2021-11-01 10:01:15 +0900

    튜플 문제풀이(노형래)

--- programmers/src/main/java/exercise/all/lvl2/tuple/nohhyungrae/Tuple.java
+++ programmers/src/main/java/exercise/all/lvl2/tuple/nohhyungrae/Tuple.java
@@ -1,8 +1,81 @@
 package exercise.all.lvl2.tuple.nohhyungrae;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Tuple {
 	
 	public static void main(String[] args) {
+		String s = "{{2,1},{2},{2,1,3},{2,1,3,4}}";
+		String s2 = "{{1,2,3},{2,1},{1,2,4,3},{2}}";
+		String s3 = "{{2,1},{2},{2,1,3},{2,1,3,4}}";
+		
+		System.out.println("Solution : "+solution(s2));
+		
 	}
+	
+	public static int[] solution(String s) {
+		int[] answer = {};
+		int index = 0;
+		List<String> minusList = new LinkedList<String>();
+		String[] arr =  s.substring(2, s.length()-2).split("\\},\\{");
+		
+		Arrays.sort(arr, new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				return s1.length() - s2.length();
+			}
+		});
+		
+		answer = new int[arr.length];
+		for(String a : arr) {
+			String[] intArr = a.split(",");
+			for(String b : intArr) {
+				if(minusList.contains(b)){
+					continue;
+				}else {
+					answer[index++] = Integer.parseInt(b);
+					minusList.add(b);
+				}
+			}
+		}
+		
+		for(int a : answer) {
+			System.out.println(a);
+		}
+		
+		return answer;
+	}
+	
+	public static int[] solution2(String s) {
+		int[] answer = {};
+		String[] arr = s.substring(2, s.length()-2).replaceAll(",", "").split("\\}"+"\\{");
+		
+		Arrays.sort(arr, new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				return s1.length() - s2.length();
+			}
+		});
+		
+		int index = 0;
+		answer = new int[arr.length];
+		List<String> minusList = new LinkedList<String>();
+		for(String a : arr) {
+			if(!minusList.isEmpty()) {
+				for(String minus : minusList) {
+					a = a.replace(minus, "");
+				}
+			}
+			answer[index] = Integer.parseInt(a);
+			minusList.add(a);
+			index++;
+		}
+		
+		return answer;
+	}
+
 
 }
